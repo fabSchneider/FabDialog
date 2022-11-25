69efe8fee1c6cd173470d2f8007f39c8b282ccc8
Fabian <mail@fabschneider.com>
2022-11-25 11:52:43 +0100

    Remove redundant packag

--- Editor/State/EditorStateGraphUpdate.cs
+++ Editor/State/EditorStateGraphUpdate.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.EditorCoroutines.Editor;
 using UnityEditor;
-using UnityEngine;
 
 namespace Fab.Dialog.Editor
 {
